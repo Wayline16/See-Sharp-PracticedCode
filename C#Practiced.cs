a6310aea44ecf8d3674f416e629980a3040fb56a
Wayline Jeffries <120781316+Wayline16@users.noreply.github.com>
2023-04-03 12:37:29 +0200

    Update C#Practiced.cs

--- C#Practiced.cs
+++ C#Practiced.cs
@@ -18,7 +18,39 @@ namespace ArraySearch
   }
 }
 
-///////////////////////////////Searching through a 2D Array///////////////////////////////
+///////////////////////////////Checking for proper positive divisors///////////////////////////////
+using System;
+using System.IO;
+using System.Linq;
+
+public class TestClass {
+    static public void Main() {
+        String line;
+        line = Console.ReadLine();
+        int T = Convert.ToInt32(line);
+        for(int t_i=0; t_i<T; t_i++) {
+            line = Console.ReadLine();
+            int N = Convert.ToInt32(line);
+
+            String out_ = Solve(N);
+            Console.Out.WriteLine(out_);
+         }
+    }
+    static String Solve(int N){
+        // Write your code here
+        int sum = 0;
+       for(int i = 1; i < N; i++){
+        if (N % i == 0){
+            sum = sum + i;
+        }
+       }
+       if (sum == N){
+        return "YES";
+        
+       }
+       else{return "NO";}
+    }
+}
 
 
 
