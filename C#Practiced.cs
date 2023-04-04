f377cdef61e1f5538b59da6409b999009ed717b1
Wayline Jeffries <120781316+Wayline16@users.noreply.github.com>
2023-04-04 12:03:19 +0200

    Update C#Practiced.cs

--- C#Practiced.cs
+++ C#Practiced.cs
@@ -52,6 +52,41 @@ public class TestClass {
     }
 }
 
+///////////////////////////Counting the longest string of numbers in a row after each other above 6 in an Array///////////////////////////////////
+//////////////////////////The function will take in a in variable and an int array/////////////////////////////////
+static int solve(int N, int[] workload)
+    {
+        int maxLength = 0;
+        int currentLength = 0;
+        int startIndex = 0;
+        
+         for (int i = 0; i < N; i++)
+        {
+            if (workload[i] > 6)
+            {
+                currentLength++;
+
+                if (currentLength > maxLength)
+                {
+                    maxLength = currentLength;
+                    startIndex = i - maxLength + 1;
+                }
+            }
+            else
+            {
+                currentLength = 0;
+            }
+        }
+
+       int a = 0;
+        for (int i = startIndex; i < startIndex + maxLength; i++)
+        {
+        	a++;
+        }
+         return a;
+      
+    }
+
 
 
 
